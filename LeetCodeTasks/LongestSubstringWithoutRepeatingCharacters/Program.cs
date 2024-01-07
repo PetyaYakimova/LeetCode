703975ac77a719b66c84f9d70853c58e2ba84418
PetyaYakimova <petya_yakimova@abv.bg>
2024-01-07 11:35:05 +0200

    Solution for the LongestSubstringWithoutRepeatingChars task

---
+++ LeetCodeTasks/LongestSubstringWithoutRepeatingCharacters/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace LongestSubstringWithoutRepeatingCharacters
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string s = Console.ReadLine();
+			Console.WriteLine(Solution.LengthOfLongestSubstring(s));
+		}
+	}
+
+	public static class Solution
+	{
+		public static int LengthOfLongestSubstring(string s)
+		{
+			int maxLength = 0;
+
+			for (int i = 0; i < s.Length; i++)
+			{
+				List<char> currentSequence = new List<char>();
+				currentSequence.Add(s[i]);
+				for (int j = i + 1; j < s.Length; j++)
+				{
+					if (currentSequence.Any(c => c == s[j]))
+					{
+						break;
+					}
+
+					currentSequence.Add(s[j]);
+				}
+
+				if (maxLength < currentSequence.Count)
+				{
+					maxLength = currentSequence.Count;
+				}
+
+				if (maxLength > s.Length - i)
+				{
+					break;
+				}
+			}
+
+			return maxLength;
+		}
+	}
+}
