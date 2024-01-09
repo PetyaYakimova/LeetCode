d9e3a74667206ff395fa13182f0466f82251c850
PetyaYakimova <petya_yakimova@abv.bg>
2024-01-09 09:26:16 +0200

    SOlution for PalindromeNumber task

---
+++ LeetCodeTasks/PalindromeNumber/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace PalindromeNumber
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int number = int.Parse(Console.ReadLine());
+			Console.WriteLine(Solution.IsPalindrome(number));
+		}
+	}
+
+	public static class Solution
+	{
+		public static bool IsPalindrome(int x)
+		{
+			if (x < 0)
+			{
+				return false;
+			}
+			if (x == 0)
+			{ 
+				return true;
+			}
+			if (x % 10 == 0)
+			{
+				return false;
+			}
+
+			int numberOfDigits = 1;
+			int copyNumber = x;
+			while (copyNumber >= 10)
+			{
+				numberOfDigits++;
+				copyNumber = copyNumber / 10;
+			}
+
+			for (int i = 0; i < numberOfDigits / 2; i++)
+			{
+				int lastDigit = x % 10;
+				int powerOf10 = (int)(Math.Pow(10, numberOfDigits - i * 2 - 1));
+				int firstDigit = x / powerOf10;
+				if (lastDigit != firstDigit)
+				{
+					return false;
+				}
+				x = x % powerOf10;
+				x = x / 10;
+			}
+
+			return true;
+		}
+	}
+}
