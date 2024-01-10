dba5611daac0fb772ff1abe162d9904cfd2c2372
PetyaYakimova <petya_yakimova@abv.bg>
2024-01-10 10:13:36 +0200

    Solution for LongestCommonPrefix task

---
+++ LeetCodeTasks/LongestCommonPrefix/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace LongestCommonPrefix
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string[] inputStrings = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+			Console.WriteLine(Solution.LongestCommonPrefix(inputStrings));
+		}
+	}
+
+	public static class Solution
+	{
+		public static string LongestCommonPrefix(string[] strs)
+		{
+			string shortestWord = strs.OrderBy(s => s.Length).FirstOrDefault();
+			string longestPrefix = "";
+			for (int i = 0; i < shortestWord.Length; i++)
+			{
+				longestPrefix = shortestWord.Substring(0, shortestWord.Length - i);
+				if (strs.Any(s => !s.StartsWith(longestPrefix)))
+				{
+					continue;
+				}
+				else
+				{
+					return longestPrefix;
+				}
+			}
+
+			return "";
+		}
+	}
+}
