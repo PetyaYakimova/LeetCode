b394492e5d59eea91288a59addfe9e4c7fa1763c
PetyaYakimova <petya_yakimova@abv.bg>
2024-01-15 09:07:00 +0200

    Solution for FindTheIndexOfTheFirstOccurenceInAString task

---
+++ LeetCodeTasks/FindTheIndexOfTheFirstOccurenceInAString/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace FindTheIndexOfTheFirstOccurenceInAString
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string haystack = Console.ReadLine();
+			string needle = Console.ReadLine();
+
+			Console.WriteLine(Solution.StrStr(haystack, needle));
+		}
+	}
+
+	public static class Solution
+	{
+		public static int StrStr(string haystack, string needle)
+		{
+			return haystack.IndexOf(needle);
+		}
+	}
+}
