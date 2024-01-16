2e6574b5101aa246a9d9e435c288786f06e9cb26
PetyaYakimova <petya_yakimova@abv.bg>
2024-01-16 11:41:15 +0200

    Solution for Search Insert Position task

---
+++ LeetCodeTasks/SearchInsertPosition/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace SearchInsertPosition
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] nums = Console.ReadLine().Split(',').Select(int.Parse).ToArray();
+			int target = int.Parse(Console.ReadLine());
+
+			Console.WriteLine(Solution.SearchInsert(nums, target));
+		}
+	}
+
+	public static class Solution
+	{
+		public static int SearchInsert(int[] nums, int target)
+		{
+			int index = 0;
+			for (int i = 0; i < nums.Length; i++)
+			{
+				if (nums[i] > target)
+				{
+					break;
+				}
+				else if (nums[i] == target)
+				{
+					index = i;
+					break;
+				}
+				else
+				{
+					index++;
+				}
+			}
+
+			return index;
+		}
+	}
+}
