32fe5a72028a730de19a95dc751af8bf5b173de8
PetyaYakimova <petya_yakimova@abv.bg>
2024-01-17 09:18:30 +0200

    Solution for First Missing Positive task

---
+++ LeetCodeTasks/FirstMissingPositive/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace FirstMissingPositive
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] nums = Console.ReadLine().Split(',').Select(int.Parse).ToArray();
+			Console.WriteLine(Solution.FirstMissingPositive(nums));
+		}
+	}
+
+	public static class Solution
+	{
+		public static int FirstMissingPositive(int[] nums)
+		{
+			for (int i = 1; i < int.MaxValue; i++)
+			{
+				if (!nums.Any(n => n == i))
+				{
+					return i;
+				}
+			}
+
+			return int.MaxValue;
+		}
+	}
+}
