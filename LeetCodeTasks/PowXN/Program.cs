110eb5d5739e050c5692b136f17b9c354d1c6791
PetyaYakimova <petya_yakimova@abv.bg>
2024-01-18 09:58:42 +0200

    Solution for Pow x(n) task

---
+++ LeetCodeTasks/PowXN/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace PowXN
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			double x = double.Parse(Console.ReadLine());
+			int n = int.Parse(Console.ReadLine());
+
+			Console.WriteLine(Solution.MyPow(x, n));
+		}
+	}
+
+	public static class Solution
+	{
+		public static double MyPow(double x, int n)
+		{
+			double result = 1.00;
+
+			if (n > 0)
+			{
+				for (int i = 1; i <= n; i++)
+				{
+					result *= x;
+				}
+			}
+			else if (n < 0)
+			{
+				for (int i = 1; i <= n * (-1); i++)
+				{
+					result /= x;
+				}
+			}
+
+			return result;
+		}
+	}
+}
