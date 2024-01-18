cb8d487f7e03356ba15f33cfab918c9d3835ca6a
PetyaYakimova <petya_yakimova@abv.bg>
2024-01-18 10:25:27 +0200

    Solution for Maximum Subarray task

---
+++ LeetCodeTasks/MaximumSubarray/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace MaximumSubarray
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] nums = Console.ReadLine().Split(',').Select(int.Parse).ToArray();
+
+			Console.WriteLine(Solution.MaxSubArray(nums));
+		}
+	}
+
+	public static class Solution
+	{
+		public static int MaxSubArray(int[] nums)
+		{
+			int maxSum = nums[0];
+			for (int i = 0; i < nums.Length; i++)
+			{
+				for (int j = 1; j <= nums.Length - i; j++)
+				{
+					int currentSum = nums.Skip(i).Take(j).Sum();
+					if (currentSum > maxSum)
+					{
+						maxSum = currentSum;
+					}
+				}
+			}
+
+			return maxSum;
+		}
+	}
+}
