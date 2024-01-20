67b9c072f3bc6cfe66f3e2b700c4a1858d91878a
PetyaYakimova <petya_yakimova@abv.bg>
2024-01-20 13:02:57 +0200

    Solution for Length of Last Word task

---
+++ LeetCodeTasks/LengthOfLastWord/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace LengthOfLastWord
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string s = Console.ReadLine();
+			Console.WriteLine(Solution.LengthOfLastWord(s));
+		}
+	}
+
+	public static class Solution
+	{
+		public static int LengthOfLastWord(string s)
+		{
+			string[] words = s.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+			return words[words.Length - 1].Length;
+		}
+	}
+}
