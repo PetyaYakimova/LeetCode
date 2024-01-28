ec091dc2bba689cb8b07c9b20db52915812f7055
PetyaYakimova <petya_yakimova@abv.bg>
2024-01-28 10:07:14 +0200

    Solution for Sqrt(x) task

---
+++ LeetCodeTasks/SqrtX/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace SqrtX
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine(Solution.MySqrt(x));
+        }
+    }
+
+    public static class Solution
+    {
+        public static int MySqrt(int x)
+        {
+            if (x == 0)
+            {
+                return 0;
+            }
+
+            int maxNumber = 1;
+            for (int i = 2; i < x; i++)
+            {
+                if (i * i == x)
+                {
+                    return i;
+                }
+                else if ((double)i * i > x)
+                {
+                    break;
+                }
+                else
+                {
+                    maxNumber = i;
+                }
+            }
+
+            return maxNumber;
+        }
+    }
+}
