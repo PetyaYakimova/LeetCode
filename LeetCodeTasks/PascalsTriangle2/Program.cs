bd40f06cf6528fb2e2017ac90a41331082422212
PetyaYakimova <petya_yakimova@abv.bg>
2024-02-04 13:20:25 +0200

    Solution for Pascals Triangle II task

---
+++ LeetCodeTasks/PascalsTriangle2/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace PascalsTriangle2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rowIndex = int.Parse(Console.ReadLine());
+            Console.WriteLine(string.Join(',', Solution.GetRow(rowIndex)));
+        }
+    }
+
+    public static class Solution
+    {
+        public static IList<int> GetRow(int rowIndex)
+        {
+            List<int> lastRow = new List<int>() { 1 };
+
+            for (int row = 1; row <= rowIndex; row++)
+            {
+                List<int> currentRow = new List<int>();
+
+                currentRow.Add(1);
+
+                for (int i = 0; i < row - 1; i++)
+                {
+                    currentRow.Add(lastRow[i] + lastRow[i + 1]);
+                }
+
+                currentRow.Add(1);
+
+                lastRow = currentRow;
+            }
+
+            return lastRow;
+        }
+    }
+}
