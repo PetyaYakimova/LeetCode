83ed120b180161015931cd982c101eaa7b2ca18f
PetyaYakimova <petya_yakimova@abv.bg>
2024-02-04 13:46:32 +0200

    Solution for Best Time to Buy and Sell Stock task

---
+++ LeetCodeTasks/BestTimeToBuyAndSellStock/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace BestTimeToBuyAndSellStock
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] prices = Console.ReadLine()
+                .Split(new char[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Console.WriteLine(Solution.MaxProfit(prices));
+        }
+    }
+
+    public static class Solution
+    {
+        public static int MaxProfit(int[] prices)
+        {
+            int maxProfit = 0;
+            for (int i = 0; i < prices.Length - 1; i++)
+            {
+                int maxValue = prices.Skip(i + 1).Max();
+                if (maxValue - prices[i] > maxProfit)
+                {
+                    maxProfit = maxValue- prices[i];
+                }
+            }
+
+            return maxProfit;
+        }
+    }
+}
