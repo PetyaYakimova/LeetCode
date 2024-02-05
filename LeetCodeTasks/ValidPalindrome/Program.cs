9e3d9ee53b5bded034f783c2bd92016af1bc3aed
PetyaYakimova <petya_yakimova@abv.bg>
2024-02-05 09:58:20 +0200

    Solution for Valid Palindrome task

---
+++ LeetCodeTasks/ValidPalindrome/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace ValidPalindrome
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+
+            Console.WriteLine(Solution.IsPalindrome(s));
+        }
+    }
+
+    public static class Solution
+    {
+        public static bool IsPalindrome(string s)
+        {
+            List<char> validLetters = new List<char>();
+            foreach (char symbol in s.ToLower())
+            {
+                if (Char.IsLetter(symbol))
+                {
+                    validLetters.Add(symbol);
+                }
+            }
+
+            for (int i = 0; i < validLetters.Count / 2; i++)
+            {
+                if (validLetters[i] != validLetters[validLetters.Count - 1 - i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
